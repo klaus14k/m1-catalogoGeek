ccdf7b063492937143a687842352694cb27f6869
Klaus Gulhote Kuhlmann <129099809+klaus14k@users.noreply.github.com>
2023-05-05 16:54:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const productsArray = [
+  // Produto 1
+  {
+    image: "./assets/img/paintings/quadro-c3po.jpg",
+    name: "Robot",
+    price: "R$250,00",
+    type: "Paintings",
+  },
+  // Produto 2
+  {
+    image: "./assets/img/actions/dragonball-action-figure.jpg",
+    name: "Goku Super Saiyajin",
+    price: "R$500,00",
+    type: "Action Figures",
+  },
+  // Produto 3 (adicione os produtos restantes abaixo)
+];
