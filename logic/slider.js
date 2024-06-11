c2fa574ce8addffe4d95e4b95c1bd2aff8d4400b
klaus14k <klauskuhlmann14@gmail.com>
2024-06-10 23:03:16 -0300

    fix: formatting code

--- logic/slider.js
+++ logic/slider.js
@@ -7,7 +7,6 @@ sliders.forEach(slider => {
     slider.addEventListener("mousedown", (e) => {
         isDown = true
         slider.classList.add("active")
-        // console.log(e.pageX)
         startX = e.pageX
         scrollLeft = slider.scrollLeft
     })
@@ -21,14 +20,11 @@ sliders.forEach(slider => {
     })
     slider.addEventListener("mousemove", (e) => {
         if (!isDown) return
-        // console.count(isDown)
         e.preventDefault()
         const x = e.pageX
-        // console.log({x, startX})
         const walk = (x - startX) * 2
-        // console.log(walk)
         slider.scrollLeft = scrollLeft - walk
     })
 })
 
-export default undefined
+export default sliders
